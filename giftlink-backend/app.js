7586982fc73f25e3c0dab0b8ba3b9c5079d63036
VanceDR <vancedelrosario@yahoo.com>
2025-03-05 21:03:21 -0500

    complete API for Gift Service

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -7,15 +7,9 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
-// ROUTES IMPORTS
-const giftRoutes = require('./routes/giftRoutes');
-
-
 const app = express();
 app.use("*",cors());
 
-// ROUTES USE
-app.use('/api/gifts', giftRoutes);
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
@@ -29,7 +23,8 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
+const giftRoutes = require('./routes/giftRoutes');
+
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
@@ -42,7 +37,7 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
