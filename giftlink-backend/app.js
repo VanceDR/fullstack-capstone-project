b8a5a7276d7e045c7e3d3918a4c21a0ae5e69c68
VanceDR <vancedelrosario@yahoo.com>
2025-03-05 22:43:54 -0500

    code MainPage and NavBar Links, tested backend and frontend

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -5,10 +5,10 @@ const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
+const { loadData } = require("./util/import-mongo/index");
 
 const app = express();
-app.use("*",cors());
+app.use("*", cors());
 
 const port = 3060;
 
@@ -49,7 +49,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Inside the server")
 })
 
